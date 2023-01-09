9a084712fb71a492d6ddc8520161426bfd145318
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-01-10 00:17:43 +0530

    added BtnGroup with parent to child and reverse communication

---
+++ components/General/BtnGroup.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="flex justify-center">
+        <button
+            :key="index"
+            type="button"
+            @click="EmitClick(option)"
+            v-for="(option, index) in options"
+            class="inline-flex items-center px-4 py-2 text-sm font-medium bg-transparent border border-gray-900 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white ml-1"
+            :class="selected == option ? 'bg-gray-900 text-white' : ''"
+        >
+            {{ option }}
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BtnGroup',
+    data: () => ({
+        selected: false,
+    }),
+    props: {
+        options: { type: Array, default: Array },
+    },
+    methods: {
+        EmitClick(option) {
+            if (option == this.selected) {
+                this.selected = false
+                this.$emit('selected', null)
+            } else {
+                this.selected = option
+                this.$emit('selected', `Option_${option}`)
+            }
+        },
+    },
+}
+</script>
