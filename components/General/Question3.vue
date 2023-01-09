443f4614cc48cd9e82618188d2b3f69de0456067
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-01-10 00:18:03 +0530

    added Question 3 component

---
+++ components/General/Question3.vue
@@ -0,0 +1,14 @@
+<template></template>
+
+<script>
+export default {
+    data: () => ({}),
+    components: {},
+    methods: {},
+    mounted() {
+        this.$nextTick(() => {})
+    },
+}
+</script>
+
+<style scoped></style>
