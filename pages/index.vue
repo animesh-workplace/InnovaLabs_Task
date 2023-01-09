04eef55497ac350a0b909109c4217f587e4f3b47
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-01-10 00:18:31 +0530

    added index page

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <GeneralBtnGroup :options="navbar_options" @selected="SwitchOption" />
+
+        <Question2 v-if="option_selected == 'Option_Question2'" />
+        <Question3 v-if="option_selected == 'Option_Question3'" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'IndexPage',
+    data: () => ({
+        option_selected: null,
+        navbar_options: ['Question 2', 'Question 3'],
+    }),
+    methods: {
+        SwitchOption(option) {
+            this.option_selected = option
+        },
+    },
+}
+</script>
