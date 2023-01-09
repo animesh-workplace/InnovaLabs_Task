7d5d57f968bad482aa12285d030af9160651f466
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-01-10 01:42:11 +0530

    added Question2 answer except the pagination

--- components/General/Question2.vue
+++ components/General/Question2.vue
@@ -1,8 +1,29 @@
-<template></template>
+<template>
+    <v-grid
+        filter
+        :source="data_summary"
+        :columns="columns"
+        class="add-height"
+    />
+</template>
 
 <script>
+import { mapFields } from 'vuex-map-fields'
+
 export default {
-    data: () => ({}),
+    data: () => ({
+        columns: [
+            { prop: 'postId', name: 'PostID', sortable: true },
+            { prop: 'id', name: 'ID', sortable: true },
+            { prop: 'email', name: 'Email', sortable: true },
+            { prop: 'name', name: 'Name', sortable: true },
+            { prop: 'body', name: 'Body', sortable: true },
+        ],
+        rows: [{ name: '1', details: 'Item 1' }],
+    }),
+    computed: {
+        ...mapFields(['data_summary']),
+    },
     components: {},
     methods: {},
     mounted() {
@@ -11,4 +32,8 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.add-height {
+    height: 50vh;
+}
+</style>
